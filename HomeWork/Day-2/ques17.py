74dc643820a524b50d454e6b09013511ac6524cd
Sakshi <139939188+sakshi900600@users.noreply.github.com>
2025-06-06 18:56:17 +0530

    ques no 17

---
+++ HomeWork/Day-2/ques17.py
@@ -0,0 +1,10 @@
+import getpass
+
+password = getpass.getpass("Enter your password: ")
+
+print("Password length is", len(password), "characters (hidden for security).")
+
+
+# output:
+# Enter your password: 
+# Password length is 5 characters (hidden for security).
