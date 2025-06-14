69c3556aeee5dadb09bb35190c9863f085963bdf
Sakshi <139939188+sakshi900600@users.noreply.github.com>
2025-06-14 21:53:23 +0530

    Project-1: Resume Validator & Formattor

---
+++ Projects/Resume_Validator/Validator.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+email = input("Enter your email: ")
+phone_no = int(input("Enter your phone no: "))
+experience = int(input("Enter your years of experience: "))
+
+
+# skills = ""
+
+# for i in skills:
+#     item = input("Enter your skills: ")
+#     skills += item
+
+print(name, email, phone_no,experience)
